aa33054c2d795f9d3dee9e9c30c1da2604816a9a
Jocelyn <ext.jrazafimaharo@enerlis.fr>
2023-10-27 15:13:55 +0300

    refactoring code

--- src/views/date/DateView.vue
+++ src/views/date/DateView.vue
@@ -1,42 +1,26 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 
 <template lang="pug">
-//- .q-mt-md.col-2(style="width: 20%")
-//-     QuDateTime(v-model="dateTime").q-mb-lg
-//-     q-btn(@click="show" color="primary" label="Primary")
 .q-mt-md.col-2
-    TestDate(v-model="dateTime")
+    QuDateTime(v-model="dateTime")
 
 .q-mt-md.col-2(style="width: 50%")
     h1 {{ dateTime }}
    
 .q-mt-md.col-2
-    TestDate(v-model="date" :withTime="false")
+    QuDateTime(v-model="date" :withTime="false")
 
 .q-mt-md.col-2(style="width: 50%")
     h1 {{ date }}
-//- .q-mt-md.col-2(style="width: 20%")
-//-     FieldDatetime(v-model="dateTime")
+
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
 import QuDateTime from './components/QuDateTime.vue';
-import InputForm from './components/InputForm.vue';
-import TestDate from './components/TestDate.vue';
-import FieldDatetime from './components/FieldDatetime.vue';
-
 
 const dateTime = ref() //'26/10/2023 11:53'
 const date = ref('')
 
-// const date = new Date()
-// const m = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
-// const dateTime = ref(date.toLocaleDateString('en-GB')+' '+date.getHours()+':'+m)
-
-
-const show = () => {
-    console.log('show: '+dateTime.value)
-}
 </script>
 <style lang="scss">
 
--- src/views/date/components/QuDateTime.vue
+++ src/views/date/components/QuDateTime.vue
@@ -1,90 +1,61 @@
 <template lang="pug">
-.q-px-sm.row.justify-between.items-center.border-bottom-color
-    q-input(:model-value="modelValue" @update:model-value="inputValue($event)" style="width: 80%" borderless :disable="props.disabled")
-    .items-center.q-gutter-xs.cursor-pointer
-        q-icon(name="today" size="25px")
-            q-popup-proxy(@before-show="updateProxy()" cover transition-show="scale" transition-hide="scale")
-                q-date(v-model='proxyDate' mask="DD/MM/YYYY" :locale="$t('locale')" today-btn @navigation="navigation($event)")
+q-input.q-pa-md(v-model="model" :rules="rules" clearable :disable="props.disabled")
+    template(v-slot:append='')
+        q-icon.cursor-pointer(name='event')
+            q-popup-proxy(@before-show="updateDate()" cover='' transition-show='scale' transition-hide='scale')
+                q-date(v-model='date' :mask="withTime ? t('datetime.mask') : t('date.mask')" :locale="locale")
                     .row.items-center.justify-end.q-gutter-sm
                         q-btn(:label="$t('btn.cancel')" color="primary" flat v-close-popup)
-                        q-btn(:label="$t('btn.validate')" color="primary" flat @click="save('date')" v-close-popup)
-        q-icon(name="schedule" size="25px")
-            q-popup-proxy(@before-show="updateProxy()" cover transition-show="scale" transition-hide="scale")
-                q-time(v-model='proxyTime' mask='HH:mm' :locale="$t('locale')" format24h)
+                        q-btn(:label="$t('btn.validate')" :disable="!date" color="primary" flat @click="save()" v-close-popup)
+
+        q-icon.cursor-pointer(name='access_time' v-if="props.withTime")
+            q-popup-proxy(@before-show="updateDate()" cover='' transition-show='scale' transition-hide='scale')
+                q-time(v-model='date' :mask="t('datetime.mask')" now-btn format24h='')
                     .row.items-center.justify-end.q-gutter-sm
                         q-btn(:label="$t('btn.cancel')" color="primary" flat v-close-popup)
-                        q-btn(:label="$t('btn.validate')" color="primary" flat @click="save('time')" v-close-popup)
+                        q-btn(:label="$t('btn.validate')" :disable="!date" color="primary" flat @click="save()" v-close-popup)  
 </template>
     <script setup lang="ts">
-    import { ref, reactive, computed } from 'vue';
+    import {ref, computed} from 'vue'
     import { useI18n } from 'vue-i18n'
     import { loadLocalMessages, i18nOption } from '../../../i18n'
-    const props = defineProps({
-        modelValue: {
-            type: String,
-            default: ''
-        },
-        disabled: {
-            type: Boolean,
-            default: false
-        }
+    //import { date } from 'quasar'
+    
+    interface Props {
+        modelValue: string
+        disabled: boolean
+        withTime: boolean
+    }
+    const props = withDefaults(defineProps<Props>(), {
+        modelValue: '',
+        disabled: false,
+        withTime: true
     })
     
     const emit = defineEmits(['update:modelValue'])
     
-    const dateIni = new Date()
-    const timeIni = `${(dateIni.getHours() < 10 ? '0' : '')}${dateIni.getHours()}:${(dateIni.getMinutes() < 10 ? '0' : '')}${dateIni.getMinutes()}` 
-    const nowIni = dateIni.toLocaleDateString('en-GB')
+    const {d, t} = useI18n()
+    const regex = new RegExp(props.withTime ? t('datetime.regex') : t('date.regex'));
     
-    // const {t,d} = useI18n(i18nOption)
-    const { t } = useI18n()
-    
-    const year = ref('')
-    const date = ref(props.modelValue.split(' ')[0])
-    const time = ref(props.modelValue.split(' ')[1])
-    
-    const proxyDate = ref(date.value)
-    const proxyTime = ref(time.value)
-    
-    const locale = reactive({
-        daysShort: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'],
-        monthsShort: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+    const date = ref('')
+    const locale = ref({
+        daysShort: t('locale.daysShort').split('_'),
+        monthsShort: t('locale.monthsShort').split('_'),
     })
     
-    const dt = computed(() => {
-        const y = year.value != undefined && year.value != '' ? year.value : date.value.split('/')[2]
-        const d = `${date.value.split('/')[0]}/${date.value.split('/')[1]}/${y}`
-        return d + ' '+ time.value
+    const model = computed({
+        get: () => props.modelValue,
+        set: (value) => emit('update:modelValue', value)
     })
     
-    const inputValue = (event: any) => {
-        date.value = event.split(' ')[0]
-        time.value = event.split(' ')[1]
-        emit('update:modelValue', event)
+    const rules = computed(() => [(val:any) => regex.test(val)])
+    
+    const save = () => {
+        model.value = date.value
     }
     
-    const updateProxy = () => {
-        proxyDate.value = props.modelValue.split(' ')[0] != '' && props.modelValue.split(' ')[0] != undefined ? props.modelValue.split(' ')[0] : nowIni
-        proxyTime.value = props.modelValue.split(' ')[1] != '' && props.modelValue.split(' ')[1] != undefined ? props.modelValue.split(' ')[1] : timeIni
+    const updateDate = () => {
+        date.value = model.value
     }
     
-    const save = (type: string) => {
-        if(type == 'date'){
-            date.value = proxyDate.value
-            time.value = proxyTime.value != undefined && proxyTime.value != '' ? proxyTime.value : timeIni
-        }else{
-            date.value = proxyDate.value != undefined && proxyDate.value != '' ? proxyDate.value : nowIni
-            time.value = proxyTime.value
-        }
-        emit('update:modelValue', dt.value)
-    }
-    
-    const navigation = (event: any) => {
-        year.value = event.year
-    }
     </script>
-    <style lang="scss">
-        .border-bottom-color {
-            border-bottom: 2px solid #BDBDBD;
-        }
-    </style>
